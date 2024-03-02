5103bf6df09c4f70f92c99fd8b09ba88252cc060
root <root@ip-172-31-47-18.ap-south-1.compute.internal>
2024-03-02 08:51:26 +0000

    5th commit

--- code.py
+++ code.py
@@ -10,5 +10,5 @@ num5 = 2.2
 sum = num1 + num2 + num3 + num4 + num5
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum + sum))
 
